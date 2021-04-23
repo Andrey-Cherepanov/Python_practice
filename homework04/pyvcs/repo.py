b9a3dd709a4592d8ac0935e792c53c000a41359a
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2021-04-23 18:40:47 +0500

    function repo_create

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -10,4 +10,25 @@ def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
     # PUT YOUR CODE HERE
-    ...
+    try:
+        if workdir.is_file():
+            raise Exception(f"{workdir} is not a directory")
+    except AttributeError:
+        workdir = pathlib.Path(wordir)
+
+    try:
+        git_dir = os.environ['GIT_DIR'] if os.environ['GIT_DIR'] else ".git"
+    except:
+        git_dir = ".git"
+
+    dir = workdir / git_dir
+    dir.mkdir()
+    (dir / "refs").mkdir()
+    (dir / "refs/heads").mkdir()
+    (dir / "refs/tags").mkdir()
+    (dir / "objects").mkdir()
+    (dir / "HEAD").write_text("ref: refs/heads/master\n")
+    (dir / "config").write_text("[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = false\n")
+    (dir / "description").write_text("Unnamed pyvcs repository.\n")
+
+    return dir
