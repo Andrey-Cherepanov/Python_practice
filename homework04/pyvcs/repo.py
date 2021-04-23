1ecfbf7d7aa771c063d5d11ac4f7fc034ece90d1
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2021-04-23 20:20:36 +0500

    function repo_find

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,12 +4,34 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    try:
+        git_dir = os.environ['GIT_DIR'] if os.environ['GIT_DIR'] else ".git"
+    except:
+        git_dir = ".git"
+
+    res_dir = None
+    workdir=pathlib.Path(workdir)
+
+    try:
+        workdir.parents
+    except:
+        raise Exception("Not a git repository")
+
+    if len(workdir.parents) == 0:
+        if not os.path.exists(workdir / git_dir):
+            raise Exception("Not a git repository")
+        return workdir.absolute() / git_dir
+    else:
+        for path in workdir.parents:
+            if git_dir in str(path):
+                res_dir = path
+    if not res_dir:
+        raise Exception("Not a git repository")
+    return res_dir
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
-    # PUT YOUR CODE HERE
+
     try:
         if workdir.is_file():
             raise Exception(f"{workdir} is not a directory")
